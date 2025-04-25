6821952ac1ca249eb2b37d43319d56a65655798f
Zisheng Ye <zisheng_ye@outlook.com>
2025-04-25 15:18:21 -0500

    fix typo and improve the performance for #3

--- python/filament/generate.py
+++ python/filament/generate.py
@@ -3,21 +3,14 @@ import numpy as np
 import scipy.spatial as KDTreed
 import time
 
+os.system("clear")
+
 def check_center_proximity(X, min_dist):
     tree = KDTreed.KDTree(X)
     d, _ = tree.query(X, k=2)
     idx = np.where(d[:, 1] > min_dist)[0]
+    
     return idx
-
-def check_intersection(X0, X, min_dist):
-    if X0.shape[0] == 0:
-        return False
-
-    tree = KDTreed.KDTree(X0)
-    d, _ = tree.query(X)
-    idx = np.where(d < min_dist)[0]
-
-    return True if idx.shape[0] > 0 else False
 
 def find_intersection(X0, X, n_chain, min_dist):
     if X0.shape[0] == 0:
@@ -27,20 +20,16 @@ def find_intersection(X0, X, n_chain, min_dist):
     d, _ = tree.query(X)
     idx = np.where(d < min_dist)[0]
 
-    return np.unique(idx//n_chain)
+    return np.unique(idx // n_chain)
 
 def remove_intersections(X_chain, n_chain, min_dist):
-  n_fiber = X_chain.shape[0] // n_chain
-  X_chain = X_chain.reshape((n_fiber, n_chain, 3))
-  for i in range(n_fiber):
-    if i >= X_chain.shape[0]:
-      break
-
-    if check_intersection(np.delete(X_chain, i, axis=0).reshape(-1, 3), X_chain[i], min_dist):
-      X_chain = np.delete(X_chain, i, axis=0)
-      i -= 1
-
-  return X_chain.reshape(-1, 3)
+    tree = KDTreed.KDTree(X_chain)
+    d, _ = tree.query(X_chain, k=2)
+    intersected_chain_idx = np.unique(np.sort(np.where(d[:, 1] < min_dist)[0] // n_chain))
+    non_intersected_chain_idx = np.setdiff1d(np.arange(X_chain.shape[0] // n_chain), intersected_chain_idx)
+    non_intersected_particle_idx = np.arange(X_chain.shape[0], dtype=np.int32).reshape(-1, n_chain)[non_intersected_chain_idx, :].flatten()
+  
+    return X_chain[non_intersected_particle_idx, :]
 
 def generate_fiber(Xc, chain):
     n_fiber = Xc.shape[0]
@@ -61,7 +50,7 @@ if __name__ == '__main__':
     np.random.seed(0)
 
     dr = 3                                                  # rest length between particles
-    n_chain = 11                                            # number of particles in a chain
+    n_chain = int(11)                                            # number of particles in a chain
     n_fiber = int(100000)                            # number of fibers
     len_fiber = dr * (n_chain - 1)                          # length of a fiber
     R = 4 * dr * 20 * 10**(1/3) * (n_fiber/(1024*10*21/11))
@@ -76,7 +65,7 @@ if __name__ == '__main__':
     remaining_fibers = n_fiber
     batch_size = int(1024)
 
-    print(f"Generating filaments...")
+    print(f"Generating filaments...\n\n")
 
     start_time = time.time()
     
@@ -88,35 +77,34 @@ if __name__ == '__main__':
         X_candidate = np.reshape(X_candidate[(r < R).nonzero(), :], (-1, 3))
 
         # remove too close centers
-        idx = check_center_proximity(X_candidate, 5 * min_dist)
+        idx = check_center_proximity(X_candidate, min_dist)
         X_candidate = np.reshape(X_candidate[idx, :], (-1, 3))
 
-        # genearate fibers
+        # generate fibers
         X_chain = generate_fiber(X_candidate, chain)
 
         # remove generated filaments having intersection
         X_chain = remove_intersections(X_chain, n_chain, min_dist)
 
-        # find and remove generatd filaments intersecting with existing filaments
-        idx_chain = find_intersection(X, X_chain, n_chain, min_dist)
-        X_chain = X_chain.reshape(-1, n_chain, 3)
-        X_chain = np.delete(X_chain, idx_chain, axis=0).reshape(-1, 3)
+        # find and remove generated filaments intersecting with existing filaments
+        idx_chain = np.setdiff1d(np.arange(X_chain.shape[0]//n_chain, dtype=np.int32), find_intersection(X[:n*n_chain, :], X_chain, n_chain, min_dist).astype(np.int32))
+        idx_chain = idx_chain[:min(idx_chain.shape[0], remaining_fibers)]
+        idx_particle = np.arange(X_chain.shape[0], dtype=np.int32).reshape(-1, n_chain)[idx_chain, :].flatten()
+        X_chain = X_chain[idx_particle, :]
         
         # save valid generated filaments
-        m = X_chain.shape[0]//n_chain if X_chain.shape[0]//n_chain <= remaining_fibers else remaining_fibers
-        X[n * n_chain:(n+m) * n_chain, :]  = X_chain[0:m * n_chain, :]
+        m = idx_chain.shape[0]
+        X[n * n_chain:(n+m) * n_chain, :] = X_chain
         remaining_fibers -= m
         n += m
-        print(f"Successfully generated {n} filaments!")
-        print(f"{remaining_fibers} filaments remain!")
-        print(".\n.\n.\n")
+        print(f"\033[2A\033[KSuccessfully generated {n:8d} filaments!\n\033[K{remaining_fibers:8d} filaments remain!", flush=True)
 
 
     end_time = time.time()
 
     elapsed_time = end_time - start_time
     print("------------------------------------------------------------------")
-    print(f"- Elapsed time: {elapsed_time:.2f} seconds")
+    print(f"- Elapsed time: {elapsed_time:6.2f} seconds")
     print("------------------------------------------------------------------")
     
     np.savetxt('initial/filament_l_'+str(n_chain)+'_'+str(n_fiber)+'.txt', X)
